0b13cf196d3d3f9bec5345dce4a6ec8f28c27fcf
Travis Reeder <treeder@gmail.com>
2016-03-31 21:43:39 -0400

    Updated to api version 0.2.17

--- JobsApi.go
+++ JobsApi.go
@@ -333,6 +333,68 @@ func (a JobsApi) JobIdLogGet (id string) (string, error) {
   return *successPayload, err
 }
 /**
+ * Update a job
+ * Typically used to update status on error/completion. TODO: only allow &#39;status&#39; field.
+ * @param id Job id
+ * @param body Job data to post
+ * @return JobWrapper
+ */
+//func (a JobsApi) JobIdPatch (id string, body JobWrapper) (JobWrapper, error) {
+func (a JobsApi) JobIdPatch (id string, body JobWrapper) (JobWrapper, error) {
+
+    _sling := sling.New().Patch(a.basePath)
+
+    // create path and map variables
+    path := "/v1/job/{id}"
+    path = strings.Replace(path, "{" + "id" + "}", fmt.Sprintf("%v", id), -1)
+
+    _sling = _sling.Path(path)
+
+    // accept header
+    accepts := []string { "application/json" }
+    for key := range accepts {
+        _sling = _sling.Set("Accept", accepts[key])
+        break // only use the first Accept
+    }
+
+// body params
+    _sling = _sling.BodyJSON(body)
+
+  var successPayload = new(JobWrapper)
+
+  // We use this map (below) so that any arbitrary error JSON can be handled.
+  // FIXME: This is in the absence of this Go generator honoring the non-2xx
+  // response (error) models, which needs to be implemented at some point.
+  var failurePayload map[string]interface{}
+
+  httpResponse, err := _sling.Receive(successPayload, &failurePayload)
+
+  if err == nil {
+    // err == nil only means that there wasn't a sub-application-layer error (e.g. no network error)
+    if failurePayload != nil {
+      // If the failurePayload is present, there likely was some kind of non-2xx status
+      // returned (and a JSON payload error present)
+      var str []byte
+      str, err = json.Marshal(failurePayload)
+      if err == nil { // For safety, check for an error marshalling... probably superfluous
+        // This will return the JSON error body as a string
+        err = errors.New(string(str))
+      }
+  } else {
+    // So, there was no network-type error, and nothing in the failure payload,
+    // but we should still check the status code
+    if httpResponse == nil {
+      // This should never happen...
+      err = errors.New("No HTTP Response received.")
+    } else if code := httpResponse.StatusCode; 200 > code || code > 299 {
+        err = errors.New("HTTP Error: " + string(httpResponse.StatusCode))
+      }
+    }
+  }
+
+  return *successPayload, err
+}
+/**
  * Retry a job.
  * The /retry endpoint can be used to force a retry of jobs with status succeeded or cancelled. It can also be used to retry jobs that in the failed state, but whose max_retries field is 0. The retried job will continue to have max_retries = 0.
  * @param id Job id
